045af7580579d77036500f61bcbf942d2d6c76c9
Ihor <tsake.love@gmail.com>
2019-11-27 13:31:11 +0200

    process.env.PORT

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-'use strict'
-const express = require('express')
-const app = express()
+'use strict';
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello,world!')
-})
-app.listen(8080, console.log('server listening...'))
+    res.send('Hello,world!');
+});
+let port = process.env.PORT;
+if (port === null || port === '') {
+    port = 8800;
+}
+app.listen(port, console.log('server listening...'));
