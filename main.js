d5b692a6d4751c0521bf27ef1bd4c24dd2129d33
Ihor <tsake.love@gmail.com>
2019-11-27 14:05:30 +0200

    changed main

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-'use strict';
-const express = require('express');
-const app = express();
+'use strict'
+const express = require('express')
+const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello,world!!!');
-});
-let port = process.env.PORT;
+  res.send('Hello,world!!!')
+})
+let port = process.env.PORT
 if (port === null || port === '') {
-    port = 8800;
+  port = 8800
 }
-app.listen(port, console.log('server listening...'));
+app.listen(port, console.log('server listening...'))
