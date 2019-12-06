f05d6e2ee8cb59a092fdf5129c48d5468fa4f88e
Ihor <tsake.love@gmail.com>
2019-12-06 10:55:02 +0200

    final

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 'use strict'
 const express = require('express')
 const app = express()
-const path = require("path")
+const path = require('path')
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
-let port = process.env.PORT || 8800
+const port = process.env.PORT || 8800
 app.listen(port, console.log('server listening...'))
